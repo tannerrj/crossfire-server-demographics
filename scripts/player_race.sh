04aad2ccecd5347e0fdbeedb285004b6fa930caa
Rick Tanner <leaf@real-time.com>
2023-11-09 17:29:33 -0600

    Total overhaul and overwrite of the player_race.sh script

--- scripts/player_race.sh
+++ scripts/player_race.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# Define the output CSV file
-output_file="results.csv"
+# Run grep commands and save results to a temporary file
+grep -h '_player' /usr/games/crossfire/var/crossfire/players/*/*.pl | sort | uniq -c | sort -n > tmp_results.txt
+grep -h 'arch pl_dragon' /usr/games/crossfire/var/crossfire/players/*/*.pl | sort | uniq -c | sort -n >> tmp_results.txt
+grep -h 'pl_half_orc' /usr/games/crossfire/var/crossfire/players/*/*.pl | sort | uniq -c | sort -n >> tmp_results.txt
 
-# Run the grep commands and save the results to CSV
-grep -h '_player' */*.pl | sort | uniq -c | sort -n > "$output_file"
-grep -h 'arch pl_dragon' */*.pl | sort | uniq -c | sort -n >> "$output_file"
-grep -h 'pl_half_orc' */*.pl | sort | uniq -c | sort -n >> "$output_file"
+# Create a CSV file from the temporary file
+awk '{print $1 "," $2}' tmp_results.txt > player_race_results.csv
 
-# Create a bar graph from the CSV using gnuplot
+# Create a bar graph using gnuplot
 gnuplot <<EOF
 set datafile separator ","
-set terminal png enhanced font 'Arial,12' size 800,600
-set output "race_bar_graph.png"
-set title "Player Races"
-set ylabel "Player Races Count"
-set xlabel "Player Races"
-set style fill solid border lt -1
+set terminal png
+set output "bar_graph.png"
+set title "Player Statistics"
+set xlabel "Count"
+set ylabel "Player Race"
 set style data histogram
-set style histogram clustered
-set boxwidth 0.9 relative
-set xtics rotate
-set key autotitle columnheader
-plot "$output_file" using 1:2:xtic(3) with boxes title "Pattern"
+set style histogram cluster
+set style fill solid border -1
+plot "player_race_results.csv" using 1:xtic(2) title ""
 EOF
 
-echo "Results saved to $output_file"
-echo "Bar graph saved to race_bar_graph.png"
+# Clean up temporary file
+rm tmp_results.txt
+
+echo "Results saved to player_race_results.csv"
+echo "Bar graph saved to player_race_bar_graph.png"
